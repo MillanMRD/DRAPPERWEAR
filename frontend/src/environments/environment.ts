7bbd79473c3751014b4a698d9d15f0a79037400e
MillanMRD <100274256+MillanMRD@users.noreply.github.com>
2023-05-29 20:52:03 +0200

    Update environment.ts

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -2,15 +2,15 @@
 
 
 
-
+/*
 export const environment = {
   production: true,
   clientId: 'AYvU7p49APJ3TWCP7EPq6Z1Sm7LijDirPdDI-G6DjNasJ2tyIVCwb0IZL1v5cKy_tw7qPr_2ybS62gCR',
   imgURL: "http://drapperweartfg.shop/storage/products/products",
   apiURL: "http://drapperweartfg.shop/api"
 };
+*/
 
-/*
 export const environment = {
   production: false,
   clientId: 'AYvU7p49APJ3TWCP7EPq6Z1Sm7LijDirPdDI-G6DjNasJ2tyIVCwb0IZL1v5cKy_tw7qPr_2ybS62gCR',
@@ -18,4 +18,3 @@ export const environment = {
   apiURL: "http://localhost:8000/api"
 
 }
-*/
